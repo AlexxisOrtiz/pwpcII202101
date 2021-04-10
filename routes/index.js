20a5fae2f98c26cc781295eed52884d0f95fe77c
AlexxisOrtiz <alexis_gayosso_ortiz@hotmail.com>
2021-04-09 22:46:17 -0500

    Agregando nombre de compañia

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express', author: 'Alexis Gayosso', appName: 'WebApp' });
+  res.render('index', { title: 'Express', author: 'Alexis Gayosso', appName: 'WebApp', company:'Awsome Software' });
 });
 
 
